01bb048a5a583078ba4658c72f98dde38083ad78
julio <julio@uma.es>
2023-11-24 18:27:51 +0100

    New test

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -8,8 +8,15 @@ public class Tests
     {
     }
 
+    
     [Test]
     public void Test1()
+    {
+        Assert.Pass();
+    }
+
+    [Test]
+    public void Test2()
     {
         Assert.Pass();
     }
